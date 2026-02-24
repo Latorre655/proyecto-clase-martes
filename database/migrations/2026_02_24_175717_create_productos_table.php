ef08dabf79f1e49984904e756cd1bf6e1bfab097
Johan Latorre <jlatorre655@unab.edu.co>
2026-02-24 14:28:50 -0400

    Creación de migraciones

---
+++ database/migrations/2026_02_24_175717_create_productos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->text("descripción")->nullable();
+            $table->decimal("price", total:8, places:2);
+            $table->timestamps();
+
+            //crear una llave foranea
+            $table->unsignedBigInteger("category_id");
+            $table->foreign("category_id")->references("id")->on("categories");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+};
