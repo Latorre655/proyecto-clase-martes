4dce828c284c0521125723eb76e1ad81b097ed20
Johan Latorre <jlatorre655@unab.edu.co>
2026-02-24 15:35:26 -0400

    Creación de factories

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+    public function definition()
+    {
+        return [
+            "name" => fake()->name(),
+            "descripcion" => fake()->paragraph(),
+            "price"=> fake()->randomFloat(2,100,100000),
+            "category_id" => Category::inRandomOrder()->first()->id
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,8 +17,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        /*
         $this->call([
             CategorySeeder::class
-        ]);
+        ]);/* */
+
+        Category::factory(200)->create();
+        Product::factory(200)->create();
     }
 }
